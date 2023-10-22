bf8ce2d97032ee160dd7a91233505172842649db
Adriano Souza <souzaadriano.contato@gmail.com>
2023-10-22 13:31:01 -0300

    feat: remove unused imports and format

--- src/core/user/adapters/hash-handler/hash-handler.contract.ts
+++ src/core/user/adapters/hash-handler/hash-handler.contract.ts
@@ -1,5 +1,3 @@
-import { Password } from '@/core/user/domain/class/password/password.class';
-
 export interface IHashHandler {
   hash(password: string): Promise<string>;
   validate(hash: string, value: string): Promise<boolean>;
